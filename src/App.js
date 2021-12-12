eedaed7bc35de9b22e7eca31d4ac305ffb371e75
stevenovak123 <stevenovak0802@gmail.com>
2021-12-12 22:12:45 +0530

    added security route to the profile and added spinner component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -15,7 +16,10 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Explore />} />
 					<Route path='/offers' element={<Offers />} />
-					<Route path='/profile' element={<Profile />} />
+
+					<Route path='/profile' element={<PrivateRoute />}>
+						<Route path='/profile' element={<Profile />} />
+					</Route>
 					<Route path='/sign-in' element={<SignIn />} />
 					<Route path='/sign-up' element={<SignUp />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,24 @@
+import { useEffect, useState, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+const useAuthStatus = () => {
+	const [loggedIn, setLoggedIn] = useState()
+	const [loading, setLoading] = useState(true)
+	const isMounted = useRef(true)
+	useEffect(() => {
+		if (isMounted) {
+			const auth = getAuth()
+			onAuthStateChanged(auth, (user) => {
+				if (user) {
+					setLoggedIn(true)
+				}
+				setLoggedIn(false)
+			})
+			return () => {
+				isMounted.current = false
+			}
+		}
+	}, [isMounted])
+	return { loggedIn, loading }
+}
+
+export default useAuthStatus
