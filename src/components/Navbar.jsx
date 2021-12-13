641f6536697b5d6678f28f05bc7b3cdcedfa9048
stevenovak123 <stevenovak0802@gmail.com>
2021-12-13 21:27:36 +0530

    added offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
 									: 'navbarListItemName'
 							}
 						>
-							Offer
+							Offers
 						</p>
 					</li>
 					<li className='navbarListItem' onClick={() => navigate('/profile')}>
