ec0ae73c124d2017806cf5eab80c134fcc99eb16
stevenovak123 <stevenovak0802@gmail.com>
2021-12-14 01:08:40 +0530

    added createListing fileuploads and added geolocation state. Set is to true for it to work with geolocation API

--- src/components/CreateListing.jsx
+++ src/components/CreateListing.jsx
@@ -1,6 +1,15 @@
 import { useState, useEffect, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import {
+	getStorage,
+	ref,
+	uploadBytesResumable,
+	getDownloadURL,
+} from 'firebase/storage'
+import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { v4 as uuidv4 } from 'uuid'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
@@ -21,7 +30,6 @@ function CreateListing() {
 		latitude: 0,
 		longitude: 0,
 	})
-
 	const {
 		type,
 		name,
@@ -37,11 +45,9 @@ function CreateListing() {
 		latitude,
 		longitude,
 	} = formData
-
 	const auth = getAuth()
 	const navigate = useNavigate()
 	const isMounted = useRef(true)
-
 	useEffect(() => {
 		if (isMounted) {
 			onAuthStateChanged(auth, (user) => {
@@ -52,27 +58,108 @@ function CreateListing() {
 				}
 			})
 		}
-
 		return () => {
 			isMounted.current = false
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [isMounted])
-
-	const onSubmit = (e) => {
+	const onSubmit = async (e) => {
 		e.preventDefault()
+		setLoading(true)
+		if (discountedPrice >= regularPrice) {
+			setLoading(false)
+			toast.error('Discounted price needs to be less than regular price')
+			return
+		}
+		if (images.length > 6) {
+			setLoading(false)
+			toast.error('Max 6 images')
+			return
+		}
+		let geolocation = {}
+		let location
+		if (geolocationEnabled) {
+			const response = await fetch(
+				`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAj1lg0Thhg1wWSABx9pe61HJteo4ysLII`
+			)
+			const data = await response.json()
+			geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+			geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+			location =
+				data.status === 'ZERO_RESULTS'
+					? undefined
+					: data.results[0]?.formatted_address
+			if (location === undefined || location.includes('undefined')) {
+				setLoading(false)
+				toast.error('Please enter a correct address')
+				return
+			}
+		} else {
+			geolocation.lat = latitude
+			geolocation.lng = longitude
+			location = address
+		}
+
+		// Store image in firebase
+		const storeImage = async (image) => {
+			return new Promise((resolve, reject) => {
+				const storage = getStorage()
+				const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+				const storageRef = ref(storage, 'images/' + fileName)
+
+				const uploadTask = uploadBytesResumable(storageRef, image)
+
+				uploadTask.on(
+					'state_changed',
+					(snapshot) => {
+						const progress =
+							(snapshot.bytesTransferred / snapshot.totalBytes) * 100
+						console.log('Upload is ' + progress + '% done')
+						switch (snapshot.state) {
+							case 'paused':
+								console.log('Upload is paused')
+								break
+							case 'running':
+								console.log('Upload is running')
+								break
+						}
+					},
+					(error) => {
+						reject(error)
+					},
+					() => {
+						// Handle successful uploads on complete
+						// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+						getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+							resolve(downloadURL)
+						})
+					}
+				)
+			})
+		}
+
+		const imgUrls = await Promise.all(
+			[...images].map((image) => storeImage(image))
+		).catch(() => {
+			setLoading(false)
+			toast.error('Images not uploaded')
+			return
+		})
+
+		console.log(imgUrls)
+
+		setLoading(false)
 	}
 
 	const onMutate = (e) => {
 		let boolean = null
-
 		if (e.target.value === 'true') {
 			boolean = true
 		}
 		if (e.target.value === 'false') {
 			boolean = false
 		}
-
 		// Files
 		if (e.target.files) {
 			setFormData((prevState) => ({
@@ -80,7 +167,6 @@ function CreateListing() {
 				images: e.target.files,
 			}))
 		}
-
 		// Text/Booleans/Numbers
 		if (!e.target.files) {
 			setFormData((prevState) => ({
@@ -89,17 +175,14 @@ function CreateListing() {
 			}))
 		}
 	}
-
 	if (loading) {
 		return <Spinner />
 	}
-
 	return (
 		<div className='profile'>
 			<header>
 				<p className='pageHeader'>Create a Listing</p>
 			</header>
-
 			<main>
 				<form onSubmit={onSubmit}>
 					<label className='formLabel'>Sell / Rent</label>
@@ -123,7 +206,6 @@ function CreateListing() {
 							Rent
 						</button>
 					</div>
-
 					<label className='formLabel'>Name</label>
 					<input
 						className='formInputName'
@@ -135,7 +217,6 @@ function CreateListing() {
 						minLength='10'
 						required
 					/>
-
 					<div className='formRooms flex'>
 						<div>
 							<label className='formLabel'>Bedrooms</label>
@@ -164,7 +245,6 @@ function CreateListing() {
 							/>
 						</div>
 					</div>
-
 					<label className='formLabel'>Parking spot</label>
 					<div className='formButtons'>
 						<button
@@ -190,7 +270,6 @@ function CreateListing() {
 							No
 						</button>
 					</div>
-
 					<label className='formLabel'>Furnished</label>
 					<div className='formButtons'>
 						<button
@@ -216,7 +295,6 @@ function CreateListing() {
 							No
 						</button>
 					</div>
-
 					<label className='formLabel'>Address</label>
 					<textarea
 						className='formInputAddress'
@@ -226,7 +304,6 @@ function CreateListing() {
 						onChange={onMutate}
 						required
 					/>
-
 					{!geolocationEnabled && (
 						<div className='formLatLng flex'>
 							<div>
@@ -253,7 +330,6 @@ function CreateListing() {
 							</div>
 						</div>
 					)}
-
 					<label className='formLabel'>Offer</label>
 					<div className='formButtons'>
 						<button
@@ -277,7 +353,6 @@ function CreateListing() {
 							No
 						</button>
 					</div>
-
 					<label className='formLabel'>Regular Price</label>
 					<div className='formPriceDiv'>
 						<input
@@ -292,7 +367,6 @@ function CreateListing() {
 						/>
 						{type === 'rent' && <p className='formPriceText'>$ / Month</p>}
 					</div>
-
 					{offer && (
 						<>
 							<label className='formLabel'>Discounted Price</label>
@@ -308,7 +382,6 @@ function CreateListing() {
 							/>
 						</>
 					)}
-
 					<label className='formLabel'>Images</label>
 					<p className='imagesInfo'>
 						The first image will be the cover (max 6).
@@ -331,5 +404,4 @@ function CreateListing() {
 		</div>
 	)
 }
-
 export default CreateListing
