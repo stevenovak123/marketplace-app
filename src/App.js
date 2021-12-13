2385fad792adbdf0764d2314c7bf116b4d8cc97b
stevenovak123 <stevenovak0802@gmail.com>
2021-12-14 00:53:14 +0530

    added a createListing section

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import CreateListing from './components/CreateListing'
 function App() {
 	return (
 		<>
@@ -25,6 +26,7 @@ function App() {
 					<Route path='/sign-in' element={<SignIn />} />
 					<Route path='/sign-up' element={<SignUp />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
+					<Route path='/create-listing' element={<CreateListing />} />
 				</Routes>
 				<Navbar />
 			</Router>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,12 @@
 import { getAuth, updateProfile } from 'firebase/auth'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { updateDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
+
 const Profile = () => {
 	const auth = getAuth()
 	const [changeDetails, setChangeDetails] = useState(false)
@@ -79,6 +82,11 @@ const Profile = () => {
 						/>
 					</form>
 				</div>
+				<Link to='/create-listing' className='createListing'>
+					<img src={homeIcon} alt='home' />
+					<p>Sell or rent your home</p>
+					<img src={arrowRight} alt='arrowRight' />
+				</Link>
 			</main>
 		</div>
 	)
