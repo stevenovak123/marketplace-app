8a2436dc9a02ed2e6874d033a43a24277173b2c8
stevenovak123 <stevenovak0802@gmail.com>
2021-12-18 11:44:12 +0530

    added delete feature to profile page to delete already listed items

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,64 @@
 import { getAuth, updateProfile } from 'firebase/auth'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { updateDoc, doc } from 'firebase/firestore'
+import {
+	updateDoc,
+	doc,
+	collection,
+	getDocs,
+	query,
+	orderBy,
+	where,
+	deleteDoc,
+} from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
 import homeIcon from '../assets/svg/homeIcon.svg'
+import ListingItem from '../components/ListingItem'
 
 const Profile = () => {
 	const auth = getAuth()
 	const [changeDetails, setChangeDetails] = useState(false)
+	const [loading, setLoading] = useState(true)
+	const [listings, setListings] = useState(null)
 	const [formData, setFormData] = useState({
 		name: auth.currentUser.displayName,
 		email: auth.currentUser.email,
 	})
+
 	const navigate = useNavigate()
+
 	const { email, name } = formData
+
+	useEffect(() => {
+		const fetchUserListings = async () => {
+			const listingsRef = collection(db, 'listings')
+			const q = query(
+				listingsRef,
+				where('userRef', '==', auth.currentUser.uid),
+				orderBy('timestamp', 'desc')
+			)
+			const querySnap = await getDocs(q)
+			let listings = []
+			querySnap.forEach((doc) => {
+				return listings.push({
+					id: doc.id,
+					data: doc.data(),
+				})
+			})
+
+			setListings(listings)
+			setLoading(false)
+		}
+		fetchUserListings()
+	}, [auth.currentUser.uid])
+
 	const onLogout = () => {
 		auth.signOut()
 		navigate('/')
 	}
+
 	const onSubmit = async () => {
 		try {
 			if (auth.currentUser.displayName !== name) {
@@ -31,13 +70,21 @@ const Profile = () => {
 			toast.error('Could not update profile details')
 		}
 	}
+
 	const onChange = (e) => {
 		setFormData((prevState) => ({
 			...prevState,
 			[e.target.id]: e.target.value,
 		}))
 	}
-
+	const onDelete = async (listingId) => {
+		if (window.confirm('Are you sure you want to delete?')) {
+			await deleteDoc(doc(db, 'listings', listingId))
+			const updatedListings = listings.filter((listing) => listing.id !== listingId	)
+			setListings(updatedListings)
+			toast.success('Sucessfully deleted Lisitng')
+		}
+	}
 	return (
 		<div className='profile'>
 			<header className='profileHeader'>
@@ -87,6 +134,22 @@ const Profile = () => {
 					<p>Sell or rent your home</p>
 					<img src={arrowRight} alt='arrowRight' />
 				</Link>
+
+				{!loading && listings?.length > 0 && (
+					<>
+						<p className='listingText'>Your Listings</p>
+						<ul className='listingsList'>
+							{listings.map((listing) => (
+								<ListingItem
+									key={listing.id}
+									listing={listing.data}
+									id={listing.id}
+									onDelete={() => onDelete(listing.id)}
+								/>
+							))}
+						</ul>
+					</>
+				)}
 			</main>
 		</div>
 	)
